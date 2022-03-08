115163825da69f9775f72b20e60f1698e70d99c7
skai-m <70408796+skai-m@users.noreply.github.com>
2022-03-08 12:03:56 -0800

    Update jobAnalysis.R

--- jobAnalysis.R
+++ jobAnalysis.R
@@ -2,9 +2,23 @@
 # Last Updated: 3/8/2022
 
 # Load packages
-library(tidyverse)
 # install.packages("cleanr")
+#install.packages("janitor")
+
+library(tidyverse)
+library(cleanr)
+library(janitor)
 
 # Load data
-jobs <- read.csv("jobs.csv", na.strings = "null", skipNul = T)
-View(jobs)
+jobs <- tibble(read.csv("jobs.csv", na.strings = "null", skipNul = T))
+# Preview tibble
+head(jobs)
+str(jobs)
+
+
+# Clean data
+jobs <- jobs %>% 
+  clean_names()
+
+jobs <- jobs %>% 
+  mutate(title = str_squish(title))
